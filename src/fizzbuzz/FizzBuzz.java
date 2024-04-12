301dd15f68e159cb1c0d6d556f24b35c62a9aaba
lanternen <leo.audouin@univ-tlse3.fr>
2024-04-12 14:00:40 +0200

    le début et la fin. En fait, on le fait pas parce qu'on est trop lents mdr

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,10 @@
+package fizzbuzz;
+
+public class FizzBuzz {
+
+	public FizzBuzz() {
+		// TODO Auto-generated constructor stub
+		System.out.println("en fait, on fait pas, parce qu'on est trop lents mdr");
+	}
+
+}
